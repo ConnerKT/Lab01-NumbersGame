1cbd9406b180c2c38da2aaa234cadbdc3d9589f7
ConnerKT <connerthompson121@gmail.com>
2023-07-07 13:53:49 -0500

    Added More Methods

--- Lab 01b/Program.cs
+++ Lab 01b/Program.cs
@@ -1,12 +1,36 @@
 ﻿// This is our Method for our Main
 class Numbersgame
 {
-    static void MainMethod(string[] args)
+    static void Main(string[] args)
     {
         // code to be executed
         StartSequence();
     }
     static void StartSequence() {
-
+        Console.WriteLine("Enter a Number Greater than Zero.");
+        int userInput = Convert.ToInt32(Console.ReadLine());
+        int[] numberArray = new int[userInput];
+        Populate(numberArray);
     }
+    static int[] Populate(int[] array)
+    {
+        
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.WriteLine("Please Enter a number " + (i+1)+"/"+ array.Length);
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        return array;
+    }
+    //static int GetSum(int[] array)
+    //{
+
+    //}
+    //static int GetProduct(int[] array, int sum)
+    //{
+    //}
+    //static decimal GetQuotient(int product)
+    //{
+
+    //}
 }
